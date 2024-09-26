84023476a8de0a835a26d8a5a66b1149b3f4fd0d
dy0110 <d.y.0110.barcelona@gmail.com>
2024-09-26 22:06:57 +0900

    ヘッダーコンポーネント追加 - daisyui追加 - tailwind-variants追加

---
+++ app/components/Header/Header.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { expect, fn, userEvent, within } from "@storybook/test";
+
+import { Header } from "./index";
+
+const meta: Meta<typeof Header> = {
+	title: "components/Header",
+	component: Header,
+	args: {
+		onClickAbout: fn(),
+		onClickBlog: fn(),
+		onClickGithub: fn(),
+		onClickHome: fn(),
+		onClickMode: fn(),
+	},
+};
+export default meta;
+
+export const Default: StoryObj<typeof Header> = {
+	play: async ({ canvasElement, args }) => {
+		const canvas = within(canvasElement);
+		const buttons = await canvas.getAllByRole("button");
+		await expect(buttons[0]).toHaveTextContent("Home");
+		await userEvent.click(buttons[0]);
+		await expect(args.onClickHome).toHaveBeenCalled();
+		await expect(buttons[1]).toHaveTextContent("About");
+		await userEvent.click(buttons[1]);
+		await expect(args.onClickAbout).toHaveBeenCalled();
+		await expect(buttons[2]).toHaveTextContent("Blog");
+		await userEvent.click(buttons[2]);
+		await expect(args.onClickBlog).toHaveBeenCalled();
+		await expect(buttons[3]).toBeInTheDocument();
+		await userEvent.click(buttons[3]);
+		await expect(args.onClickGithub).toHaveBeenCalled();
+		await expect(buttons[4]).toBeInTheDocument();
+		await userEvent.click(buttons[4]);
+		await expect(args.onClickMode).toHaveBeenCalled();
+	},
+};
---
+++ app/components/Header/index.tsx
@@ -0,0 +1,51 @@
+import { tv } from "tailwind-variants";
+import { GithubLine, MoonLine } from "../Icons";
+
+const headerStyle = tv({
+	slots: {
+		content: "flex items-center gap-8 justify-between",
+		buttonStyle: "btn btn-ghost btn-md",
+		iconStyle: "btn btn-circle btn-sm",
+	},
+});
+
+type Props = {
+	onClickHome: () => void;
+	onClickAbout: () => void;
+	onClickBlog: () => void;
+	onClickGithub: () => void;
+	onClickMode: () => void;
+};
+
+export function Header({
+	onClickAbout,
+	onClickBlog,
+	onClickGithub,
+	onClickHome,
+	onClickMode,
+}: Props) {
+	const { content, buttonStyle, iconStyle } = headerStyle();
+	return (
+		<header className="h-[84px] w-full py-1 px-7 flex items-center justify-between bg-base-100">
+			<div className={content()}>
+				<button type="button" className={buttonStyle()} onClick={onClickHome}>
+					Home
+				</button>
+				<button type="button" className={buttonStyle()} onClick={onClickAbout}>
+					About
+				</button>
+				<button type="button" className={buttonStyle()} onClick={onClickBlog}>
+					Blog
+				</button>
+			</div>
+			<div className={content()}>
+				<button type="button" className={iconStyle()} onClick={onClickGithub}>
+					<GithubLine className="text-base-content" />
+				</button>
+				<button type="button" className={iconStyle()} onClick={onClickMode}>
+					<MoonLine className="text-base-content" />
+				</button>
+			</div>
+		</header>
+	);
+}
