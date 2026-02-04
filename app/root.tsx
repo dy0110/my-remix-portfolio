e729d544fa672df5d1b994fc89dd662024b8ae90
dy0110 <d.y.0110.barcelona@gmail.com>
2026-02-05 00:51:21 +0900

    フォントを M PLUS 1p に変更した

--- app/root.tsx
+++ app/root.tsx
@@ -26,7 +26,7 @@ export const links: LinksFunction = () => [
 	},
 	{
 		rel: "stylesheet",
-		href: "https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap",
+		href: "https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@100;300;400;500;700;800;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap",
 	},
 ];
 
